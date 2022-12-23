5fda1ff889f8caecbc16c6f3469ccf5c3c60aa99
Sergio Llopis <serllobel@alu.edu.gva.es>
2022-12-23 23:48:19 +0100

    Segon commit – nou métode

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        mostrarMensaje();
+    }
+    public static void mostrarMensaje() {
+        System.out.println("Feliz navidad!!!");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Felices Fiestas!!");
+        }
     }
 }
